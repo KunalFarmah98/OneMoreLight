607c124091211b8ec17ade9f7a8740373a8e38f9
KUNAL FARMAH <kunalfarmah98@gmail.com>
2020-10-25 01:54:06 +0530

    fixed gradle issues.

--- app/src/main/java/com/apps/kunalfarmah/onemorelight/SplashActivity.kt
+++ app/src/main/java/com/apps/kunalfarmah/onemorelight/SplashActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.FirebaseApp
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        FirebaseApp.initializeApp(applicationContext)
         var handler =Handler()
         handler.postDelayed(Runnable {
             startActivity(Intent(this,MainActivity::class.java))
